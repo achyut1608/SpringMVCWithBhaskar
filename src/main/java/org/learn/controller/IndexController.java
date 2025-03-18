04bb62f4fdcb0e4d307c370af337eaacae9d412e
Achyut <achyut1608@gmail.com>
2025-03-18 20:40:22 +0530

    spring MVC learning init Project configuration

---
+++ src/main/java/org/learn/controller/IndexController.java
@@ -0,0 +1,17 @@
+package org.learn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/",method = RequestMethod.GET)
+public class IndexController {
+
+    @GetMapping("home")
+    public String home(){
+        return "index";
+    }
+
+}
